551dff7fc030968b45a622d8b439db24ae90e2f9
codemazter <er.gopiselvam@gmail.com>
2016-08-28 15:18:17 +0800

    sdsd
    sdsdsd

--- js/all.js
+++ js/all.js
@@ -14,9 +14,35 @@ $(document).ready(function(e) {
 });
 
 function gotFS(fileSystem) {
-   alert('gotFS');
+   //alert('gotFS');
     //console.log("got filesystem: "+fileSystem.name); // displays "persistent"
     window.rootFS = fileSystem.root;
     alert(window.rootFS.toURL());
+
+    var fileTransfer = new FileTransfer();
+    var fileURL = window.rootFS.toURL()+"downloaded-image.png";
+    var uri = 'http://cordova.apache.org/static/img/cordova_bot.png';
+
+    fileTransfer.download(
+        uri,
+        fileURL,
+        function (entry) {
+            alert("Successful download...");
+            alert("download complete: " + entry.toURL());
+            
+        },
+        function (error) {
+            alert("download error source " + error.source);
+            alert("download error target " + error.target);
+            alert("upload error code" + error.code);
+        },
+        null, // or, pass false
+        {
+            //headers: {
+            //    "Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+            //}
+        }
+    );
+
 }
 
