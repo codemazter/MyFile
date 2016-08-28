43582eeafa817e0874e9851eb8126b89fff5ee2b
codemazter <er.gopiselvam@gmail.com>
2016-08-28 16:00:56 +0800

    asas
    asas

--- js/all.js
+++ js/all.js
@@ -2,35 +2,28 @@ $.support.cors = true;
 $(document).ready(function(e) {
   //=========================== Device Ready ==================================
     document.addEventListener("deviceready", function() {
-
-      alert('deviceready');
     window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, function(){
       console.log("error requesting LocalFileSystem");
     });
-       
     }, false);
-    /** Device Ready ends **/
 });
 
 function gotFS(fileSystem) {
-
-  var uri = 'http://cordova.apache.org/static/img/cordova_bot.png';
     window.rootFS = fileSystem.root;
     fileSystem.root.getDirectory('test1', { create: true });
+}
 
-    var filePath = fileSystem.root.toURL() + 'test1/' + uri.split('/').pop();
+function download() {
 
-    alert(window.rootFS.toURL()+" --\n"+filePath);
+    var uri = 'http://cordova.apache.org/static/img/cordova_bot.png';
 
+    var filePath = window.rootFS.toURL() + 'test1/' + uri.split('/').pop();
+    alert("filePath: "+filePath);
     var fileTransfer = new FileTransfer();
-    //var fileURL = window.rootFS.toURL()+"downloaded-image.png";
-    
-
     fileTransfer.download(uri,filePath, function (entry) {
           alert("Successful download...");
           alert("download complete: " + entry.toURL());
-            
         },
         function (error) {
             alert("download error source " + error.source);
@@ -44,6 +37,4 @@ function gotFS(fileSystem) {
             //}
         }
     );
-
 }
-
