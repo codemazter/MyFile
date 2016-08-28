8fcb8a7a4659f7e5b6dfcc4fc08bb9dd225687d3
codemazter <er.gopiselvam@gmail.com>
2016-08-28 15:03:42 +0800

    sdsd
    sdsd

--- js/all.js
+++ js/all.js
@@ -2,8 +2,20 @@ $.support.cors = true;
 $(document).ready(function(e) {
   //=========================== Device Ready ==================================
     document.addEventListener("deviceready", function() {
-      alert('deviceready');
+
+    window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, function(){
+      console.log("error requesting LocalFileSystem");
+    });
        
     }, false);
     /** Device Ready ends **/
 });
+
+function gotFS(fileSystem) {
+    console.log("got filesystem: "+fileSystem.name); // displays "persistent"
+    window.rootFS = fileSystem.root;
+
+    console(window.rootFS.toURL());
+}
+
